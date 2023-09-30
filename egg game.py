5be61c9633bb2a3c7f60d3e56058c04747257083
Mariah Abid <118097369+mariah16265@users.noreply.github.com>
2023-09-30 11:39:10 +0400

    Update egg game.py

--- egg game.py
+++ egg game.py
@@ -24,15 +24,21 @@ difficulty = 0.95
 catcher_color = "blue"
 catcher_width = 100
 catcher_height = 100
+#These variables define the initial position and boundaries of the catcher on the canvas.
+#calculates the x-coordinate of the top-left corner of the catcher.
 catcher_startx = canvas_width / 2 - catcher_width / 2
 catcher_starty = canvas_height - catcher_height - 20
+#calculates the x-coordinate of the bottom-right corner of the catcher by adding the catcher's width to catcher_startx.
 catcher_startx2 = catcher_startx + catcher_width
 catcher_starty2 = catcher_starty + catcher_height
-
+#following lines initialize and configure the catcher object (representing the player-controlled object) and set up a font for displaying text within the game:
 catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
 game_font = font.nametofont("TkFixedFont")
 game_font.config(size=18)
 
+#This line creates a text object (score_text) on the canvas (c) using the create_text method.
+#The parameters used in the create_text method are as follows:
+#10, 10: These are the coordinates (x, y) where the text will be displayed on the canvas. 
 
 score = 0
 score_text = c.create_text(10, 10, anchor="nw", font=game_font, fill="darkblue", text="Score: "+ str(score))
@@ -42,6 +48,7 @@ lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fil
 
 eggs = []
 
+#Responsible for creating eggs (objects) in the game at random positions and intervals.
 def create_egg():
     x = randrange(10, 740)
     y = 40
@@ -51,9 +58,12 @@ def create_egg():
 
 def move_eggs():
     for egg in eggs:
+#c.coords(egg) returns a tuple containing the (x, y) coordinates of the top-left and bottom-right corners of the egg bounding rectangle. The variables eggx, eggy, eggx2, and eggy2 are used to unpack these coordinates.        
         (eggx, eggy, eggx2, eggy2) = c.coords(egg)
         c.move(egg, 0, 10)
         if eggy2 > canvas_height:
+#If the if condition is met, meaning the egg has fallen below the lower edge of the canvas, the egg_dropped function is called with the egg as an argument.
+#This function is responsible for handling what happens when an egg reaches the bottom of the canvas.            
             egg_dropped(egg)
     root.after(egg_speed, move_eggs)
 
